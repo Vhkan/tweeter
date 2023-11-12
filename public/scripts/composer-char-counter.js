b6a52b49d38f15924a46040097b5f30257c26825
Vhkan <vhkan@yahoo.com>
2023-11-12 11:12:29 -0700

    Add composer-char-counter.js file, add tweets input field hadler, count symbols functionality, change symbols style if its quantity exceeds 140 characters

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+//Settting the character counter
+$(document).ready(function() {
+  console.log("Link works");
+  $('#tweet-text').on('input', function() {
+    const tweet = $(this).val();
+    let tweetLength = tweet.length;
+    let charLeft = 140 - tweetLength;
+
+    //Updating the counter display
+    $('#counter').text(charLeft);
+    //Applying styles on remaining symbols
+    if (charLeft < 0) {
+      $('#counter').css('color', 'red');
+    } else {
+      $('#counter').css('color', '#45474B')
+    }
+  });
+});
